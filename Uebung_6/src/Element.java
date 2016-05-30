e60987a12fbed1ba0c693957aebdb85a4f841b76
DevCodeOne <christian.r.dev@googlemail.com>
2016-05-30 23:19:21 +0200

    6. Uebung hinzugefuegt

---
+++ Uebung_6/src/Element.java
@@ -0,0 +1,119 @@
+public class Element implements Comparable {
+
+    private static String[] attributeNames = {"Matrikelnummer", "Vorname", "Nachname", "Hobby"};
+    private static int numberOfAttributes = attributeNames.length;
+    private static int[] sortOrder = {0};
+
+    private static long compareCounter = 0;
+
+    private int matNr;
+    private String vName;
+    private String nName;
+    private String hobby;
+
+    public Element(int matNr, String vName, String nName, String hobby) {
+        this.matNr = matNr;
+        this.vName = vName;
+        this.nName = nName;
+        this.hobby = hobby;
+    }
+
+    public Element() {
+        this(0, "Max", "Mustermann", "Surfen");
+    }
+
+    public int getMatNr() {
+        return matNr;
+    }
+
+    public void setMatNr(int matNr) {
+        this.matNr = matNr;
+    }
+
+    public String getVName() {
+        return vName;
+    }
+
+    public void setVName(String name) {
+        vName = name;
+    }
+
+    public String getNName() {
+        return nName;
+    }
+
+    public void setNName(String name) {
+        nName = name;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public static String[] getAttributeNames() {
+        return attributeNames;
+    }
+
+    public static int getNumberOfAttributes() {
+        return numberOfAttributes;
+    }
+
+    public static void setSortOrder(int[] sortOrder) {
+        Element.sortOrder = sortOrder;
+    }
+
+    public static String getSortOrderNames() {
+        String s = "";
+        for (int i = 0; i < sortOrder.length; i++)
+            s += attributeNames[sortOrder[i]] + " ";
+        return s;
+    }
+
+    public static long getCompareCounter() {
+        return compareCounter;
+    }
+
+    public static void setCompareCounter(long compareCounter) {
+        Element.compareCounter = compareCounter;
+    }
+
+    public String toString() {
+        return (matNr + " : " + vName + " " + nName + ", " + hobby);
+    }
+
+    public int compareTo(Object o) {
+        // negativ falls dieses Objekt kleiner als o ist
+        // 0       falls dieses Objekt gleich o ist
+        // positiv falls dieses Objekt größer als o ist
+
+        compareCounter++;
+
+        int res = 0;
+        for (int i = 0; i < sortOrder.length; i++) {
+            switch(sortOrder[i]) {
+                case 0 :
+                    res = Integer.compare(matNr, ((Element) o).matNr);
+                    break;
+                case 1 :
+                    res = vName.compareTo(((Element) o).vName);
+                    break;
+                case 2 :
+                    res = nName.compareTo(((Element) o).nName);
+                    break;
+                case 3 :
+                    res = hobby.compareTo(((Element) o).hobby);
+                    break;
+            }
+
+            if (res != 0)
+                return res; // alternativ auch einfach break
+        }
+        return res;
+
+
+    }
+}
---
+++ Uebung_6/src/List.java
@@ -0,0 +1,88 @@
+public class List {
+
+    private Element L[];
+    private int size;
+    private int nextIndex;
+
+    public List(int size) {
+        L = new Element[size];
+        this.size = size;
+        nextIndex = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean addElement(Element elem) {
+        if (nextIndex < size) {
+            L[nextIndex] = elem;
+            nextIndex++;
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < nextIndex; i++) {
+            s += L[i] + "\n";
+        }
+        return s;
+    }
+
+    public void selectionSort(boolean aufsteigend) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                int res = L[i].compareTo(L[j]) * (aufsteigend ? 1 : -1);
+                if (res > 0) {
+                    Element tmp = L[j];
+                    for (int k = j; k > i; k--) {
+                        L[k] = L[k - 1];
+                    }
+                    L[i] = tmp;
+                }
+            }
+        }
+    }
+
+    public void bubbleSort(boolean aufsteigend) {
+        for (int i = 0; i < size; i++) {
+            boolean swap = false;
+            for (int j = 0; j < size - 1; j++) {
+                int res = L[j].compareTo(L[j + 1]) * (aufsteigend ? 1 : -1);
+                if (res > 0) {
+                    Element tmp = L[j];
+                    L[j] = L[j + 1];
+                    L[j + 1] = tmp;
+                    swap = true;
+                }
+            }
+
+            if (!swap)
+                break;
+        }
+    }
+
+    public void insertionSort(boolean aufsteigend) {
+        for (int i = 1; i < size; i++) {
+            int j = 0;
+            for (; j < i; j++) {
+                int res = L[i].compareTo(L[j]) * (aufsteigend ? -1 : 1);
+                if (res > 0) {
+                    break;
+                }
+            }
+
+            Element tmp = L[i];
+
+            for (int k = i; k > j; k--) {
+                L[k] = L[k - 1];
+            }
+
+            L[j] = tmp;
+        }
+    }
+
+
+}
